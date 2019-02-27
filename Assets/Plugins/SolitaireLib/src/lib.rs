1d2624779a6f1c32d2ca30a88890584a537c9f0b
David J. Felix <felix.davidj@gmail.com>
2019-02-27 00:29:09 -0500

    feat get tests working. start on init game

--- Assets/Plugins/SolitaireLib/src/lib.rs
+++ Assets/Plugins/SolitaireLib/src/lib.rs
@@ -2,6 +2,47 @@ use std;
 use rand;
 use core::iter::Zip;
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn standard_deck_has_52_cards() {
+        let deck = create_standard_deck();
+        assert_eq!(deck.len(), 52);
+    }
+
+    #[test]
+    fn standard_deck_has_unique_cards() {
+        let mut deck = create_standard_deck();
+        deck.sort();
+        deck.dedup();
+        assert_eq!(deck.len(), 52);
+    }
+
+    #[test]
+    fn shuffled_deck_has_same_number_of_cards() {
+        let deck = create_standard_deck();
+        let shuffled_deck = shuffle_deck(&deck);
+
+        assert_eq!(deck.len(), shuffled_deck.len());
+    }
+
+    #[test]
+    fn new_game_has_unique_cards() {}
+
+    #[test]
+    fn new_game_has_correct_layout() {
+        let game = GameState::new();
+        assert_eq!(game.tableau[0].face_up.len(), 1);
+    }
+}
+
 #[derive(Debug, Clone, Copy, Eq, PartialEq, Ord, PartialOrd)]
 enum Color {
     Black,
@@ -39,9 +80,15 @@ struct Card {
     suit: Suit,
 }
 
+#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd)]
+struct TableauPile {
+    pub face_down: Vec<Card>,
+    pub face_up: Vec<Card>,
+}
+
 #[derive(Debug, Clone)]
 struct GameState {
-    tableau: [Vec<Card>; 7],
+    tableau: [TableauPile; 7],
     foundation: [Vec<Card>; 4],
     hand: Vec<Card>,
     waste: Vec<Card>,
@@ -98,16 +145,66 @@ fn parse_suit(suit: &str) -> Result<Suit, &'static str> {
     }
 }
 
-fn shuffle_deck(deck: Vec<Card>) -> Vec<Card> {
-    let mut order: Vec<(u64, &Card)> = vec!(0..52)
-        .iter()
+//impl std::iter::FromIterator<(&u64, &Card> for (u64, &Card) {
+//}
+
+fn shuffle_deck(deck: &Vec<Card>) -> Vec<Card> {
+    let mut order: Vec<(u64, &Card)> = (0..52)
         .map(|_| -> u64{ rand::random::<u64>() })
         .zip(deck.iter())
         .collect();
     order.sort_by_key(|(order, _)| -> u64{ *order });
-    order.iter().map(|(_, card)| -> Card { *card.clone() }).collect()
+    order.iter()
+        .map(|(_, &card)| -> Card { card.clone() })
+        .collect()
 }
 
 trait Game {
     fn new() -> Self;
+}
+
+impl Game for GameState {
+    fn new() -> Self {
+        let deck = shuffle_deck(&create_standard_deck());
+        GameState {
+            tableau: [
+                TableauPile {
+                    face_down: Vec::new(),
+                    face_up: deck[0..1].to_vec(),
+                },
+                TableauPile {
+                    face_down: deck[1..2].to_vec(),
+                    face_up: deck[2..3].to_vec(),
+                },
+                TableauPile {
+                    face_down: deck[3..5].to_vec(),
+                    face_up: deck[5..6].to_vec(),
+                },
+                TableauPile {
+                    face_down: deck[6..9].to_vec(),
+                    face_up: deck[9..10].to_vec(),
+                },
+                TableauPile {
+                    face_down: deck[10..14].to_vec(),
+                    face_up: deck[14..15].to_vec(),
+                },
+                TableauPile {
+                    face_down: deck[15..20].to_vec(),
+                    face_up: deck[20..21].to_vec(),
+                },
+                TableauPile {
+                    face_down: deck[21..28].to_vec(),
+                    face_up: deck[28..29].to_vec(),
+                },
+            ],
+            foundation: [
+                Vec::new(),
+                Vec::new(),
+                Vec::new(),
+                Vec::new(),
+            ],
+            hand: deck[29..].to_vec(),
+            waste: Vec::new(),
+        }
+    }
 }
